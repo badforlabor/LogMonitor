731dbae3d6175954ba89c60589979d5fdc8825e9
badlabor <badlabor@work.com>
2018-02-02 18:30:35 +0800

    1

--- LogMonitor/LogMonitorForm.Designer.cs
+++ LogMonitor/LogMonitorForm.Designer.cs
@@ -28,18 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogMonitorForm));
+            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.metroTabPage1 = new MetroFramework.Controls.MetroTabPage();
+            this.fctb = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.metroProgressSpinner3 = new MetroFramework.Controls.MetroProgressSpinner();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.cmbLogLevel = new MetroFramework.Controls.MetroComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.metroTabControl1.SuspendLayout();
+            this.metroTabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fctb)).BeginInit();
             this.SuspendLayout();
+            // 
+            // metroTabControl1
+            // 
+            this.metroTabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroTabControl1.Controls.Add(this.metroTabPage1);
+            this.metroTabControl1.CustomBackground = false;
+            this.metroTabControl1.FontSize = MetroFramework.MetroTabControlSize.Medium;
+            this.metroTabControl1.FontWeight = MetroFramework.MetroTabControlWeight.Light;
+            this.metroTabControl1.Location = new System.Drawing.Point(8, 57);
+            this.metroTabControl1.Name = "metroTabControl1";
+            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.Size = new System.Drawing.Size(781, 520);
+            this.metroTabControl1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTabControl1.StyleManager = null;
+            this.metroTabControl1.TabIndex = 1;
+            this.metroTabControl1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.metroTabControl1.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTabControl1.UseStyleColors = false;
+            // 
+            // metroTabPage1
+            // 
+            this.metroTabPage1.Controls.Add(this.fctb);
+            this.metroTabPage1.CustomBackground = false;
+            this.metroTabPage1.HorizontalScrollbar = false;
+            this.metroTabPage1.HorizontalScrollbarBarColor = true;
+            this.metroTabPage1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.HorizontalScrollbarSize = 10;
+            this.metroTabPage1.Location = new System.Drawing.Point(4, 36);
+            this.metroTabPage1.Name = "metroTabPage1";
+            this.metroTabPage1.Size = new System.Drawing.Size(773, 480);
+            this.metroTabPage1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTabPage1.StyleManager = null;
+            this.metroTabPage1.TabIndex = 0;
+            this.metroTabPage1.Text = "Logcat";
+            this.metroTabPage1.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTabPage1.VerticalScrollbar = false;
+            this.metroTabPage1.VerticalScrollbarBarColor = true;
+            this.metroTabPage1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroTabPage1.VerticalScrollbarSize = 10;
+            // 
+            // fctb
+            // 
+            this.fctb.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fctb.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.fctb.AutoScrollMinSize = new System.Drawing.Size(154, 14);
+            this.fctb.BackBrush = null;
+            this.fctb.CharHeight = 14;
+            this.fctb.CharWidth = 8;
+            this.fctb.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fctb.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.fctb.IsReplaceMode = false;
+            this.fctb.Location = new System.Drawing.Point(0, 3);
+            this.fctb.Name = "fctb";
+            this.fctb.Paddings = new System.Windows.Forms.Padding(0);
+            this.fctb.ReadOnly = true;
+            this.fctb.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.fctb.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("fctb.ServiceColors")));
+            this.fctb.ShowLineNumbers = false;
+            this.fctb.Size = new System.Drawing.Size(777, 481);
+            this.fctb.TabIndex = 2;
+            this.fctb.Text = "fastColoredTextBox1";
+            this.fctb.Zoom = 100;
+            // 
+            // metroProgressSpinner3
+            // 
+            this.metroProgressSpinner3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroProgressSpinner3.CustomBackground = false;
+            this.metroProgressSpinner3.Location = new System.Drawing.Point(760, 58);
+            this.metroProgressSpinner3.Maximum = 100;
+            this.metroProgressSpinner3.Name = "metroProgressSpinner3";
+            this.metroProgressSpinner3.Size = new System.Drawing.Size(28, 28);
+            this.metroProgressSpinner3.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroProgressSpinner3.StyleManager = null;
+            this.metroProgressSpinner3.TabIndex = 12;
+            this.metroProgressSpinner3.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroProgressSpinner3.Value = 50;
+            // 
+            // metroTextBox2
+            // 
+            this.metroTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroTextBox2.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.metroTextBox2.FontWeight = MetroFramework.MetroTextBoxWeight.Regular;
+            this.metroTextBox2.Location = new System.Drawing.Point(578, 58);
+            this.metroTextBox2.Multiline = false;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.Size = new System.Drawing.Size(171, 28);
+            this.metroTextBox2.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox2.StyleManager = null;
+            this.metroTextBox2.TabIndex = 13;
+            this.metroTextBox2.Text = "Styled Textbox";
+            this.metroTextBox2.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox2.UseStyleColors = true;
+            // 
+            // cmbLogLevel
+            // 
+            this.cmbLogLevel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbLogLevel.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmbLogLevel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLogLevel.FontSize = MetroFramework.MetroLinkSize.Medium;
+            this.cmbLogLevel.FontWeight = MetroFramework.MetroLinkWeight.Regular;
+            this.cmbLogLevel.FormattingEnabled = true;
+            this.cmbLogLevel.ItemHeight = 23;
+            this.cmbLogLevel.Items.AddRange(new object[] {
+            "Normal Combobox"});
+            this.cmbLogLevel.Location = new System.Drawing.Point(371, 57);
+            this.cmbLogLevel.Name = "cmbLogLevel";
+            this.cmbLogLevel.Size = new System.Drawing.Size(197, 29);
+            this.cmbLogLevel.Style = MetroFramework.MetroColorStyle.Blue;
+            this.cmbLogLevel.StyleManager = null;
+            this.cmbLogLevel.TabIndex = 14;
+            this.cmbLogLevel.Theme = MetroFramework.MetroThemeStyle.Light;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // LogMonitorForm
             // 
             this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.cmbLogLevel);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.metroProgressSpinner3);
+            this.Controls.Add(this.metroTabControl1);
             this.Location = new System.Drawing.Point(0, 0);
             this.MaximizeBox = false;
             this.Name = "LogMonitorForm";
+            this.metroTabControl1.ResumeLayout(false);
+            this.metroTabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fctb)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroTabPage metroTabPage1;
+        private MetroFramework.Controls.MetroProgressSpinner metroProgressSpinner3;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroComboBox cmbLogLevel;
+        private FastColoredTextBoxNS.FastColoredTextBox fctb;
+        private System.Windows.Forms.Timer timer1;
     }
 }
